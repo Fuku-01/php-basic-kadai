2af492408a811d81c7d075208bc1eb6f795c262e
Fuku-01 <ry.fukushige@gmail.com>
2025-10-30 04:31:31 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>PHPの課題を提出しよう</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      // 連想配列 $Array に値を代入する
+      $Array = ['name'=> 'onion','price'=>200 ,'weight'=>160];
+
+      // 連想配列 $Array の値を出力する。
+      print_r($Array);
+      ?>
+    </p>
+  </body>
+</html>
