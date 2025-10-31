583e0cf646565260aedda0963bea1454e2cf05ad
Fuku-01 <ry.fukushige@gmail.com>
2025-11-01 07:17:10 +0900

    kadai_sortの内容を修正

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -7,21 +7,21 @@
   <body>
     <p>
       <?php
-      function sort_2way($nums,$array){
-        if($array === true){
+      function sort_2way($array,$order){
+      
+        if($order === true){
           echo '昇順にソートします。<br>';
-          sort($nums);
-          foreach($nums as $num){
+          sort($array);
+          foreach($array as $num){
             echo $num .'<br>';
           }
         }else {
           echo '降順にソートします。<br>';
-          rsort($nums);
-          foreach($nums as $num){
+          rsort($array);
+          foreach($array as $num){
             echo $num . '<br>';
           }
         }
-
       }
 
       $nums = [15, 4, 8, 23, 10];
